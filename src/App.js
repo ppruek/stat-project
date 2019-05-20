974bcbe791967b11539952401584e6dfa5b9b0ae
Arniwatt Chonkiattipoom <arniwatt.c@ku.th>
2019-05-20 19:56:25 +0700

    add drag to zoom

--- src/App.js
+++ src/App.js
@@ -481,26 +481,37 @@ class App extends Component {
                           chartType="LineChart"
                           loader={<div>Loading Chart</div>}
                           data={[
-                            ['x', 'dogs', 'cats'],
-                            [0, 0, 0],
-                            [1, 10, 5],
-                            [2, 23, 15],
-                            [3, 17, 9],
-                            [4, 18, 10],
-                            [5, 9, 5],
-                            [6, 11, 3],
-                            [7, 27, 19],
+                            ['Year', 'Budget'],
+                              [2547,147880473],
+                              [2548,169898087],
+                              [2549,263863673],
+                              [2550,418245615],
+                              [2551,384968940],
+                              [2552,172820590],
+                              [2553,473329500],
+                              [2554,1212111538],
+                              [2555,2373592334],
+                              [2556,10520593054],
+                              [2557,1205452833],
+                              [2558,750505423],
+                              [2559,572014200],
+                              [2560,631396600],
+                              [2561,1417028200],
+                              [2562,1348902100]
                           ]}
                           options={{
                             hAxis: {
-                              title: 'Time',
+                              title: 'Year',
                             },
                             vAxis: {
-                              title: 'Popularity',
+                              title: 'Budget',
                             },
                             series: {
                               1: { curveType: 'function' },
                             },
+                            explorer: {
+                              actions: ['dragToZoom', 'rightClickToReset'] 
+                            }
                           }}
                           rootProps={{ 'data-testid': '2' }}
                         />
@@ -510,39 +521,43 @@ class App extends Component {
                       this.state.valueGDP === 1 && <TabContainer>
                         <Chart
                           width={'645px'}
-                          height={'400px'}
-                          chartType="GeoChart"
+                          height={'375px'}
+                          chartType="LineChart"
+                          loader={<div>Loading Chart</div>}
                           data={[
-                            ['Country', 'Tax'], 
-                            ['Møre og Romsdal',7393.3859],
-                            ['Hordaland',4315.36539938],
-                            ['Buskerud',0],
-                            ['Vest-Agder',1767.67542],
-                            ['Oslo',4036.86470145],
-                            ['Rogaland',7632.98150937],
-                            ['Vestfold',1608.2962],
-                            ['Troms',0],
-                            ['Trøndelag',8260.7089865],
-                            ['Akershus',6119.7423],
-                            ['Aust-Agder',4132.61105],
-                            ['Finnmark',1973.769],
-                            ['Hedmark',3739.87455],
-                            ['Nordland',3530.86155],
-                            ['Oppland',2671.18505],
-                            ['Sogn og Fjordane',0],
-                            ['Telemark',0],
-                            ['Østfold',0]
+                            ['Year', 'Budget'],
+                            [2547,179746356],
+                            [2548,263620717],
+                            [2549,191971794],
+                            [2550,755304377],
+                            [2551,1037951132],
+                            [2552,696334408],
+                            [2553,491465457],
+                            [2554,533679994],
+                            [2555,259682095],
+                            [2556,181257454],
+                            [2557,170764123],
+                            [2558,347882511],
+                            [2559,565876700],
+                            [2560,835560110],
+                            [2561,1075204800],
+                            [2562,574015000]
                           ]}
                           options={{
-                            region: 'NO',
-                            resolution: 'provinces',
-                            colorAxis: { colors: ['#DBB3B1', '#C89FA3', '#A67F8E', '#6C534E', '#2C1A1D'] },
-                            backgroundColor: '#88d2ff',
-                            datalessRegionColor: '#ffffff',
-                            defaultColor: '#f5f5f5',
+                            hAxis: {
+                              title: 'Year',
+                            },
+                            vAxis: {
+                              title: 'Budget',
+                            },
+                            series: {
+                              1: { curveType: 'function' },
+                            },
+                            explorer: {
+                              actions: ['dragToZoom', 'rightClickToReset'] 
+                            }
                           }}
-                          mapsApiKey="AIzaSyCXU_3IGQ9ZlHDeJuOoGJXYjsjPCaBbcm8"
-                          rootProps={{ 'data-testid': '4' }}
+                          rootProps={{ 'data-testid': '2' }}
                         />
                       </TabContainer>
                     }
@@ -550,25 +565,43 @@ class App extends Component {
                       this.state.valueGDP === 2 && <TabContainer>
                         <Chart
                           width={'645px'}
-                          height={'400px'}
-                          chartType="GeoChart"
+                          height={'375px'}
+                          chartType="LineChart"
+                          loader={<div>Loading Chart</div>}
                           data={[
-                            ['Country', 'Tax'], 
-                            ['Scotland', 7346.66381466667],
-                            ['Northern Ireland', 3687.868244],
-                            ['Wales', 21870.095728],
-                            ['England', 9250.23191270588]
+                            ['Year', 'Budget'],
+                            [2547,152145509],
+                            [2548,185228819],
+                            [2549,210426275],
+                            [2550,259519332],
+                            [2551,308408961],
+                            [2552,335746300],
+                            [2553,241746962],
+                            [2554,994382445],
+                            [2555,2787440897],
+                            [2556,1596719613],
+                            [2557,1696189523],
+                            [2558,802258751],
+                            [2559,1132860440],
+                            [2560,956056500],
+                            [2561,1509440800],
+                            [2562,1734170800]
                           ]}
                           options={{
-                            region: 'GB',
-                            resolution: 'provinces',
-                            colorAxis: { colors: ['#DBB3B1', '#C89FA3', '#A67F8E', '#6C534E', '#2C1A1D'] },
-                            backgroundColor: '#88d2ff',
-                            datalessRegionColor: '#ffffff',
-                            defaultColor: '#f5f5f5',
+                            hAxis: {
+                              title: 'Year',
+                            },
+                            vAxis: {
+                              title: 'Budget',
+                            },
+                            series: {
+                              1: { curveType: 'function' },
+                            },
+                            explorer: {
+                              actions: ['dragToZoom', 'rightClickToReset'] 
+                            }
                           }}
-                          mapsApiKey="AIzaSyCXU_3IGQ9ZlHDeJuOoGJXYjsjPCaBbcm8"
-                          rootProps={{ 'data-testid': '4' }}
+                          rootProps={{ 'data-testid': '2' }}
                         />
                       </TabContainer>
                     }
@@ -576,69 +609,43 @@ class App extends Component {
                       this.state.valueGDP === 3 && <TabContainer>
                         <Chart
                           width={'645px'}
-                          height={'400px'}
-                          chartType="GeoChart"
+                          height={'375px'}
+                          chartType="LineChart"
+                          loader={<div>Loading Chart</div>}
                           data={[
-                            ['Country', 'Tax', { role: "tooltip", type: "string", p: { html: true } }], 
-                            ['DK-81', 25240.325852, '<b>Nordjylland</b><br>Tax: <b>25240.325852</b>'],
-                            ['DK-82', 20698.11990379, '<b>Midtjylland</b><br>Tax: <b>20698.11990379</b>'],
-                            ['DK-84', 22839.76838402, '<b>Hovedstaden</b><br>Tax: <b>22839.76838402</b>'],
-                            ['DK-83', 20976.16213112, '<b>Sjælland</b><br>Tax: <b>20976.16213112</b>'],
-                            ['DK-85', 20458.81082, '<b>Syddanmark</b><br>Tax: <b>20458.81082</b>'],
+                            ['Year', 'Budget'],
+                            [2547,140489245],
+                            [2548,152876356],
+                            [2549,225105450],
+                            [2550,325981056],
+                            [2551,246307501],
+                            [2552,322617884],
+                            [2553,240094310],
+                            [2554,351570080],
+                            [2555,165842496],
+                            [2556,151886439],
+                            [2557,219011843],
+                            [2558,255223560],
+                            [2559,210003240],
+                            [2560,231266400],
+                            [2561,325144000],
+                            [2562,543389400]
                           ]}
                           options={{
-                            region: 'DK',
-                            resolution: 'provinces',
-                            colorAxis: { colors: ['#DBB3B1', '#C89FA3', '#A67F8E', '#6C534E', '#2C1A1D'] },
-                            backgroundColor: '#88d2ff',
-                            datalessRegionColor: '#ffffff',
-                            defaultColor: '#f5f5f5',
-                            tooltip: {isHtml: true},
+                            hAxis: {
+                              title: 'Year',
+                            },
+                            vAxis: {
+                              title: 'Budget',
+                            },
+                            series: {
+                              1: { curveType: 'function' },
+                            },
+                            explorer: {
+                              actions: ['dragToZoom', 'rightClickToReset'] 
+                            }
                           }}
-                          mapsApiKey="AIzaSyCXU_3IGQ9ZlHDeJuOoGJXYjsjPCaBbcm8"
-                          rootProps={{ 'data-testid': '4' }}
-                        />
-                      </TabContainer>
-                    }
-                    {
-                      this.state.valueGDP === 4 && <TabContainer>
-                        <Chart
-                          width={'645px'}
-                          height={'400px'}
-                          chartType="GeoChart"
-                          data={[
-                            ['Country', 'Tax', { role: "tooltip", type: "string", p: { html: true } }], 
-                            ['FI-18',3300.9375, '<b>Uusimaa</b><br>Tax: <b>3300.9375</b>'],
-                            ['FI-13',836, '<b>Pohjois-Karjala</b><br>Tax: <b>836</b>'],
-                            ['FI-08',2181.5, '<b>Keski-Suomi</b><br>Tax: <b>2181.5</b>'],
-                            ['FI-09',8270.25, '<b>Kymenlaakso</b><br>Tax: <b>8270.25</b>'],
-                            ['FI-15',8, '<b>Pohjois-Savo</b><br>Tax: <b>8</b>'],
-                            ['FI-16',1664, '<b>Päijät-Häme</b><br>Tax: <b>1664</b>'],
-                            ['FI-02',732.5, '<b>Etelä-Karjala</b><br>Tax: <b>732.5</b>'],
-                            ['FI-14',6591.5, '<b>Pohjois-Pohjanmaa</b><br>Tax: <b>6591.5</b>'],
-                            ['FI-11',3550.4125, '<b>Pirkanmaa</b><br>Tax: <b>3550.4125</b>'],
-                            ['FI-19',2996, '<b>Varsinais-Suomi</b><br>Tax: <b>2996</b>'],
-                            ['FI-12',652.6325, '<b>Pohjanmaa</b><br>Tax: <b>652.6325</b>'],
-                            ['FI-01',3220.4, '<b>Ahvenanmaan maakunta</b><br>Tax: <b>3220.4</b>'],
-                            ['FI-04',8, '<b>Etelä-Savo</b><br>Tax: <b>8</b>'],
-                            ['FI-05',8, '<b>Kainuu</b><br>Tax: <b>8</b>'],
-                            ['FI-06',2694.624225, '<b>Kanta-Häme</b><br>Tax: <b>2694.624225</b>'],
-                            ['FI-07',8, '<b>Keski-Pohjanmaa</b><br>Tax: <b>8</b>'],
-                            ['FI-10',8, '<b>Lappi</b><br>Tax: <b>8</b>'],
-                            ['FI-17',8, '<b>Satakunta</b><br>Tax: <b>8</b>'],
-                            ['FI-03',8, '<b>Etelä-Pohjanmaa</b><br>Tax: <b>8</b>'],
-                          ]}
-                          options={{
-                            region: 'FI',
-                            resolution: 'provinces',
-                            colorAxis: { colors: ['#DBB3B1', '#C89FA3', '#A67F8E', '#6C534E', '#2C1A1D'] },
-                            backgroundColor: '#88d2ff',
-                            datalessRegionColor: '#ffffff',
-                            defaultColor: '#f5f5f5',
-                            tooltip: {isHtml: true},
-                          }}
-                          mapsApiKey="AIzaSyCXU_3IGQ9ZlHDeJuOoGJXYjsjPCaBbcm8"
-                          rootProps={{ 'data-testid': '4' }}
+                          rootProps={{ 'data-testid': '2' }}
                         />
                       </TabContainer>
                     }
@@ -653,11 +660,10 @@ class App extends Component {
                       scrollButtons="auto"
                       style={{display: 'flex', margin: 'auto'}}
                     >
-                      <Tab label="Sweden" />
-                      <Tab label="Norway" />
-                      <Tab label="United Kingdom" />
-                      <Tab label="Denmark" />
-                      <Tab label="Finland" />
+                      <Tab label="Chonburi" />
+                      <Tab label="Rayong" />
+                      <Tab label="Chanthaburi" />
+                      <Tab label="Trat" />
                     </Tabs>
                   </AppBar>
                   </Card>
