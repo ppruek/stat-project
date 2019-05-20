9c09c91416248a0512f4b59a4e3ba8c7a52ece9a
Arniwatt Chonkiattipoom <arniwatt.c@ku.th>
2019-05-20 17:35:03 +0700

    add build

---
+++ build/precache-manifest.9036af6751bfdfd5fac124032946d38f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ad97665d9dfd6abcd293",
+    "url": "/ppruek/ppruek.github.io/static/js/runtime~main.dc6b568f.js"
+  },
+  {
+    "revision": "320b42704cb4c079c747",
+    "url": "/ppruek/ppruek.github.io/static/js/main.25a06e64.chunk.js"
+  },
+  {
+    "revision": "f043a2c210e66353ec08",
+    "url": "/ppruek/ppruek.github.io/static/js/2.29bcedca.chunk.js"
+  },
+  {
+    "revision": "320b42704cb4c079c747",
+    "url": "/ppruek/ppruek.github.io/static/css/main.2f379192.chunk.css"
+  },
+  {
+    "revision": "bfd663f879c7fe3d54c7ff76befd2c61",
+    "url": "/ppruek/ppruek.github.io/index.html"
+  }
+];
